d95955ad5fbb4459ac32abdc237a9a115eb83f90
loilunhp007 <63707576+loilunhp007@users.noreply.github.com>
2021-04-19 23:37:52 +0700

    Revert "Register User and  create angular Auth-Guard"
    This reverts commit 76dc0c1d2c0123aaddf3a1a9e03f82a7238db30b.

--- src/AppiumTest/Demo.java
+++ src/AppiumTest/Demo.java
@@ -2,18 +2,10 @@ package AppiumTest;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.Action;
-
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.By.ByXPath;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.touch.TouchActions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -21,18 +13,11 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileDriver;
-import io.appium.java_client.MobileElement;
-import io.appium.java_client.TouchAction;
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.android.AndroidTouchAction;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
-import io.appium.java_client.touch.TapOptions;
-import io.appium.java_client.touch.offset.ElementOption;
-import io.appium.java_client.touch.offset.PointOption;
+
 public class Demo {
-  public AppiumDriver<MobileElement> driver;
+  public AppiumDriver driver;
 	@BeforeMethod()
 	public void setUp() throws MalformedURLException {
 		
@@ -42,47 +27,48 @@ public class Demo {
 		caps.setCapability(MobileCapabilityType.DEVICE_NAME,"Samsung Galaxy J6" );
 		caps.setCapability(MobileCapabilityType.AUTOMATION_NAME,"UIAutomator2" );
 		caps.setCapability(MobileCapabilityType.BROWSER_NAME, "Chrome");
-		URL url = new URL("http://0.0.0.0:4723/wd/hub");
-		driver = new AppiumDriver<MobileElement>(url,caps);
+		URL url = new URL("http://0.0.0.0:4723/wd/hub"); 
+		driver = new AppiumDriver(url,caps);
+		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 	}
 	@Test(priority = 1)
 	public void assertUsernameAndPasswordField() {
 
-		String expectedValue="input";
-		String usernameField=driver.findElement(By.xpath("//input[@id='txtUsername']")).getTagName();
-		String passwordField=driver.findElement(By.xpath("//input[@id='txtPassword']")).getTagName();
-		Assert.assertEquals(usernameField,expectedValue);
-		Assert.assertEquals(passwordField, expectedValue);
+		String ExpectedUsernamefield="txtUsername";
+		String ExpectedPasswordfield="txtPassword";
+		String usernameField=driver.findElement(By.xpath("//div[@id='divUsername']/input[@id='txtUsername']")).getText();
+		String passwordField=driver.findElement(By.xpath("//div[@id='divPassword']/input[@id='txtPassword']")).getText();
+		System.out.println(usernameField+"aa:"+passwordField);
+		//Assert.assertEquals(usernameField, ExpectedUsernamefield);
+		//Assert.assertEquals(passwordField, ExpectedPasswordfield);
 		driver.close();
 	}
 	
-	@SuppressWarnings("deprecation")
 	@Test(priority = 2)
 	public void loginSuccess() throws InterruptedException{
 		//check login vs URL
 		String expectedURL="https://opensource-demo.orangehrmlive.com/index.php/dashboard";
 		String username="Admin";
 		String password="admin123";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
 		String URL = driver.getCurrentUrl();
 		Assert.assertEquals(URL, expectedURL);
 		driver.close();
 	}
-	
 	@Test(priority = 3)
 	public void loginWithUppercaseUsername() throws InterruptedException {
 		String expectedMessage="Csrf token validation failed";
 		String username="AdMiN";
 		String password="admin123";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
-		String message = driver.findElement(By.xpath("//span[@id='spanMessage']")).getText();
+		String message = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(message, expectedMessage);
 		driver.close();
 		
@@ -93,11 +79,11 @@ public class Demo {
 		String expectedMessage="Csrf token validation failed";
 		String username="Admin";
 		String password="Admin123";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
-		String message = driver.findElement(By.xpath("//span[@id='spanMessage']")).getText();
+		String message = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(message, expectedMessage);
 		driver.close();
 		
@@ -106,13 +92,13 @@ public class Demo {
 	@Test(priority = 5)
 	public void loginWithSpecialCharacter() throws InterruptedException {
 		String expectedMessage=" Invalid credentials";
-		String username=" /*Admin";
+		String username=" */Admin";
 		String password="admin123";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
-		String message = driver.findElement(By.xpath("//span[@id='spanMessage']")).getText();
+		String message = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(message, expectedMessage);
 		driver.close();
 	}
@@ -121,11 +107,11 @@ public class Demo {
 		String expectedMessage=" Invalid credentials";
 		String username="Admin123";
 		String password="admin123";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
-		String message = driver.findElement(By.xpath("//span[@id='spanMessage']")).getText();
+		String message = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(message, expectedMessage);
 		driver.close();
 	}
@@ -134,11 +120,11 @@ public class Demo {
 		String expectedMessage=" Invalid credentials";
 		String username="Admin";
 		String password="admin123456";
-		driver.findElement(By.xpath("//input[@id='txtUsername']")).sendKeys(username);
-		driver.findElement(By.xpath("//input[@id='txtPassword']")).sendKeys(password);
-		driver.getKeyboard().sendKeys(Keys.ENTER);
+		driver.findElement(By.id("txtUsername")).sendKeys(username);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.id("btnLogin")).click();
 		Thread.sleep(3000);
-		String message = driver.findElement(By.xpath("//span[@id='spanMessage']")).getText();
+		String message = driver.findElement(By.id("spanMessage")).getText();
 		Assert.assertEquals(message, expectedMessage);
 		driver.close();
 		
